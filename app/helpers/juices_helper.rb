999f40c197266484404eaf4562552f7d235e677e
Bill Kolokithas <kolokithas.b@gmail.com>
2015-09-12 02:27:43 +0300

    hide "more ingredients" button when the list is empty, make juice titles clickable and fix a bug when given an empty filter url parameter

--- app/helpers/juices_helper.rb
+++ app/helpers/juices_helper.rb
@@ -1,5 +1,11 @@
 module JuicesHelper
   def filter_add_remove(params, filter)
-    params.merge(filter: filter) { |_, old, new| old.include?(new) ? [old].flatten - [new] : [old, new].flatten }
+    params.merge(filter: filter) do |_, old, new|
+      if old.try(:include?, new)
+        [old].flatten - [new] # remove ingredient
+      else
+        old.blank? ? new : [old, new].flatten
+      end
+    end
   end
 end
